6d89e32c2f317f09c11099dd8f514e3dc70ad3bc
devhhhk <hhhk0815@gmail.com>
2023-11-14 16:20:20 +0900

    섹션5: 6강

--- src/App.js
+++ src/App.js
@@ -1,10 +1,61 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+import { useState, useRef } from 'react';
+
+// const dummyList = [
+//   {
+//     id:1,
+//     author:"GDSC",
+//     content:"Hi",
+//     emotion:5,
+//     created_date: new Date().getTime()
+//   },
+//   {
+//     id:2,
+//     author:"Hi-arc",
+//     content:"Bye",
+//     emotion:4,
+//     created_date: new Date().getTime()
+//   },
+//   {
+//     id:3,
+//     author:"COK",
+//     content:"Hi",
+//     emotion:5,
+//     created_date: new Date().getTime()
+//   },
+//   {
+//     id:4,
+//     author:"Attract",
+//     content:"Bye",
+//     emotion:4,
+//     created_date: new Date().getTime()
+//   },
+// ]
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id : dataId.current
+    }
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  }
+
   return (
     <div className="App">
-      <DiaryEditor></DiaryEditor>
+      <DiaryEditor onCreate={onCreate}></DiaryEditor>
+      <DiaryList diaryList={data}></DiaryList>
     </div>
   );
 }
--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,10 @@
-import {useState} from 'react';
+import React, {useRef, useState} from 'react';
 
-const DiaryEditor = () => {
+const DiaryEditor = ({onCreate}) => {
+
+    const authorInput = useRef();
+    const contentInput = useRef();
+
     const [state, setState] = useState({
         author: "",
         content: "",
@@ -15,15 +19,36 @@ const DiaryEditor = () => {
     };
 
     const handleSubmit = () => {
+        if (state.author.length < 1) {
+            // alert('작성자는 최소 1글자 이상 입력해주세요.');
+            // focus
+            authorInput.current.focus();
+            return ;
+        }
+        if (state.content.length < 5) {
+            // alert('일기 본문은 최소 5글자 이상 입력해주세요.');
+            // focus
+            contentInput.current.focus();
+            return ;
+        }
+
+        onCreate(state.author, state.content, state.emotion)
         console.log(state);
         alert('저장 성공!');
-    }
+
+        setState({
+            author:"",
+            content:"",
+            emotion:""
+        });
+    };
 
     return (
         <div className="DiaryEditor">
             <h2>오늘의 일기</h2>
             <div>
                 <input 
+                ref={authorInput}
                 value={state.author} 
                 onChange={handleChangeState}
                 name="author"
@@ -32,6 +57,7 @@ const DiaryEditor = () => {
             </div>
             <div>
                 <textarea 
+                ref={contentInput}
                 value={state.content} 
                 onChange={handleChangeState}
                 name='content'
